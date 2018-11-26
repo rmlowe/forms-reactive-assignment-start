18845bf56afe5598a82d4f522c359a098cd8289d
Robert Lowe <robertmlowe@rmlowe.com>
2018-11-26 09:23:38 +0800

    Assignment 7: Practicing Reactive Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+  projectStatuses = ['Stable', 'Critical', 'Finished'];
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, Validators.required, this.noTestAsync),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl('Stable')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+  }
+
+  // noTest(control: FormControl): {[s: string]: boolean} {
+  //   if (control.value === 'Test') {
+  //     return {'noTest': true};
+  //   }
+  //   return null;
+  // }
+
+  noTestAsync(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise<any>((resolve, reject) => {
+      const v = control.value;
+      setTimeout(() => {
+        if (v === 'Test') {
+          resolve({'noTest': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+  }
 }
